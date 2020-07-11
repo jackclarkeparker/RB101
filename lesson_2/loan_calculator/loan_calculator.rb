cfd4ed7bc041adecf39623408d63a024f5f414fb
Jack Clarke-Parker <jackclarkeparker@gmail.com>
2020-07-11 16:14:32 +1200

    improved/refactored code as per code review advice

--- lesson_2/loan_calculator/loan_calculator.rb
+++ lesson_2/loan_calculator/loan_calculator.rb
@@ -1,5 +1,21 @@
+# TODO:
+# *** Assign summary report of loan to a key in a hash, make it available to
+# compare various loans on screen at the same time.
+# *** Exit loop with just yes, no, y, or n. give prompt indicating this & error
+# message in case of invalid input.
+# *** Make APR input only available to receive Integer? Maybe naht.
+
 require 'yaml'
 MESSAGES = YAML.load_file('loan_calculator_messages.yml')
+
+def newline
+  puts "\n"
+end
+
+def pause
+  sleep(2.5)
+  newline
+end
 
 def prompt(message)
   puts "=> #{message}"
@@ -21,82 +37,112 @@ def two_dp(num)
   format("%.2f", num)
 end
 
-prompt MESSAGES["welcome"]
-sleep(1)
-
-loop do
-  prompt MESSAGES['loan_prompt']
-
-  loan = ""                      # LOAN INPUT
+def get_loan_amount
   loop do
     loan = gets.chomp
 
     if number?(loan)
       loan = loan.to_f
-      break
+      break loan
     else
       prompt MESSAGES['invalid_loan']
+      pause
     end
   end
+end
 
-  prompt MESSAGES['apr_prompt']
-  apr = ""                       # APR INPUT
+def apr_number_check
   loop do
     apr = gets.chomp
     apr.delete("%")
 
     if integer?(apr)
-      apr = apr.to_f / 100
+      break apr.to_f / 100
     elsif float?(apr)
-      apr = apr.to_f
+      break apr.to_f
     else
       prompt MESSAGES['invalid_apr_1']
-      next
-    end
-
-    if apr.between?(0.01, 0.3)
-      break
-    else
-      prompt MESSAGES['invalid_apr_2']
+      pause
     end
   end
+end
 
-  prompt MESSAGES['duration_prompt']
+def get_apr
+  loop do
+    apr = apr_number_check
 
-  months = ""                    # DURATION INPUT
+    if apr.between?(0.01, 0.3)
+      break apr
+    else
+      prompt MESSAGES['invalid_apr_2']
+      pause
+    end
+  end
+end
+
+def get_month_duration
   loop do
     months = gets.chomp
 
     if integer?(months)
       months = months.to_i
-      break
+      break months
     else
       prompt MESSAGES['invalid_duration']
+      pause
     end
   end
-  sleep(1)
+end
 
-  monthly_interest = apr / 12    # CALCULATIONS
-
+def calculations(loan, apr, months)
+  monthly_interest = apr / 12
   monthly_payment = loan *
                     (monthly_interest /
                     (1 - (1 + monthly_interest)**(-months)))
-
   monthly_payment = two_dp(monthly_payment)
   loan = two_dp(loan)
 
+  _to_be_assigned = [monthly_payment, loan]
+end
+
+def display_result(monthly_payment, loan, apr, months)
   prompt <<-MSG
 Your monthly payment will be $#{monthly_payment}.
    Loan - $#{loan}
-   APR - #{apr * 100}%
+   APR - #{two_dp(apr * 100)}%
    Duration - #{months} months
   MSG
-  sleep(4)
+  sleep(3)
+end
 
-  prompt MESSAGES['another_calculation?']
+newline
+prompt MESSAGES["welcome"]                                  # BEGINNING
+sleep(1)
+
+loop do
+  newline
+
+  prompt MESSAGES['loan_prompt']
+  loan = get_loan_amount                                    # LOAN INPUT
+  newline
+
+  prompt MESSAGES['apr_prompt']
+  apr = get_apr                                             # APR INPUT
+  newline
+
+  prompt MESSAGES['duration_prompt']
+  months = get_month_duration                               # DURATION INPUT
+  newline
+  sleep(1)
+
+  monthly_payment, loan = calculations(loan, apr, months)   # CALCULATIONS
+  display_result(monthly_payment, loan, apr, months)        # DISPLAY RESULT
+
+  newline
+  prompt MESSAGES['another_calculation?']                   # CALCULATE AGAIN?
   loop_again = gets.chomp
-
   break unless loop_again.downcase.start_with?("y")
 end
 
 prompt MESSAGES['goodbye']
+newline
