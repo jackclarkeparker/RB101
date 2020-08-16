954aec89eeb0574d7cc26352fc3bf6bc8ae56d26
Jack Clarke-Parker <jackclarkeparker@gmail.com>
2020-08-17 08:00:12 +1200

    improvements following suggestions

--- lesson_2/rock_paper_scissors/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -2,11 +2,21 @@ require 'yaml'
 
 MESSAGES = YAML.load_file("rock_paper_scissors_messages.yml")
 VALID_CHOICES = %w(rock paper scissors spock lizard)
-NUMBER_TO_MOVE = { 1 => 'rock', 2 => 'paper', 3 => 'scissors',
-                   4 => 'spock', 5 => 'lizard' }
-WIN = { 'rock' => %w(lizard scissors), 'paper' => %w(rock spock),
-        'scissors' => %w(paper lizard), 'spock' => %w(scissors rock),
-        'lizard' => %w(spock paper) }
+CODE_FOR_MOVE = {
+  'r' => 'rock',
+  'p' => 'paper',
+  'sc' => 'scissors',
+  'sp' => 'spock',
+  'l' => 'lizard'
+}
+WIN = {
+  'rock' => %w(lizard scissors),
+  'paper' => %w(rock spock),
+  'scissors' => %w(paper lizard),
+  'spock' => %w(scissors rock),
+  'lizard' => %w(spock paper)
+}
+WINNING_SCORE = 5
 
 def prompt(message)
   puts "=> #{message}"
@@ -14,8 +24,16 @@ end
 
 def print_welcome
   system("clear")
-  prompt MESSAGES["welcome"]
+  prompt MESSAGES["welcome1"]
   sleep(1.2)
+  prompt MESSAGES["welcome2"]
+  sleep(1.8)
+  print MESSAGES["welcome3"]
+  sleep(0.8)
+  print MESSAGES["welcome4"]
+  sleep(1)
+  print MESSAGES["welcome5"]
+  sleep(1.6)
 end
 
 def get_player_choice
@@ -24,67 +42,64 @@ def get_player_choice
     choice = gets.chomp
     sleep(0.7)
 
-    if ('1'..'5').include?(choice)
+    if %w(r p sc sp l).include?(choice)
       break
     else
-      prompt MESSAGES["invalid_number"]
+      prompt MESSAGES["invalid_choice"]
     end
   end
 
-  NUMBER_TO_MOVE[choice.to_i]
+  CODE_FOR_MOVE[choice]
 end
 
-def tally_scores(player, computer, scores)
-  if WIN[player].include?(computer)
-    scores[0] += 1
-  elsif WIN[computer].include?(player)
-    scores[1] += 1
+def tally_scores(player, comp, scores)
+  if WIN[player].include?(comp)
+    scores[:player] += 1
+  elsif WIN[comp].include?(player)
+    scores[:comp] += 1
   end
 
   scores
 end
 
-def print_results(player, computer, scores)
-  if WIN[player].include?(computer)
+def print_results(player, comp, scores)
+  if WIN[player].include?(comp)
     prompt "You won!"
-  elsif WIN[computer].include?(player)
+  elsif WIN[comp].include?(player)
     prompt 'Computer won!'
   else
     prompt "It's a tie!"
   end
   sleep(0.4)
 
-  prompt "Player score ~ #{scores[0]}"
-  prompt "Computer score ~ #{scores[1]}"
+  prompt "Player score ~ #{scores[:player]}"
+  prompt "Computer score ~ #{scores[:comp]}"
   sleep(0.3)
 end
 
 def someone_grand_winner?(scores)
-  scores.any? { |score| score == 5 }
+  scores.any? { |(_, score)| score == WINNING_SCORE }
 end
 
 def print_grand_winner(scores)
-  if scores[0] == 5
+  if scores[:player] == WINNING_SCORE
     prompt MESSAGES["player_g_win"]
-  elsif scores[1] == 5
-    prompt MESSAGES["computer_g_win"]
+  elsif scores[:comp] == WINNING_SCORE
+    prompt MESSAGES["comp_g_win"]
   end
   sleep(1.5)
 end
 
 def play_again?
+  answer = ''
   loop do
     answer = gets.chomp.downcase
     sleep(0.7)
 
-    if answer == 'y'
-      break true
-    elsif answer == 'n'
-      break false
-    else
-      prompt MESSAGES['invalid_answer']
-    end
+    break if %w(y n).include?(answer)
+    prompt MESSAGES['invalid_answer']
   end
+  answer == 'y'
 end
 
 def reset_game
@@ -95,19 +110,19 @@ def reset_game
   system("clear")
 end
 
-scoreboard = [0, 0]
+scoreboard = { player: 0, comp: 0 }
 print_welcome
 
 loop do
   puts MESSAGES["input_prompt"]
   choice = get_player_choice
-  computer_choice = VALID_CHOICES.sample
+  comp_choice = VALID_CHOICES.sample
 
-  prompt "You chose: #{choice}, computer chose #{computer_choice}"
+  prompt "You chose: #{choice}, computer chose #{comp_choice}"
   sleep(1)
 
-  scoreboard = tally_scores(choice, computer_choice, scoreboard)
-  print_results(choice, computer_choice, scoreboard)
+  scoreboard = tally_scores(choice, comp_choice, scoreboard)
+  print_results(choice, comp_choice, scoreboard)
 
   next unless someone_grand_winner?(scoreboard)
   print_grand_winner(scoreboard)
@@ -117,7 +132,7 @@ loop do
 
   break unless again
   reset_game
-  scoreboard = [0, 0]
+  scoreboard = { player: 0, comp: 0 }
 end
 
 prompt MESSAGES['goodbye']
