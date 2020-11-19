61745443133ec4fe21ebd18f925e152b9a53aa70
Jack Clarke-Parker <jackclarkeparker@gmail.com>
2020-11-20 11:55:18 +1300

    Refactored STARTING_DECK constant, made small adjustments to user prompts to indicate single letter input options

--- lesson_6/21/21.rb
+++ lesson_6/21/21.rb
@@ -1,20 +1,7 @@
 require 'yaml'
 
-STARTING_DECK = [
-  2, 2, 2, 2,
-  3, 3, 3, 3,
-  4, 4, 4, 4,
-  5, 5, 5, 5,
-  6, 6, 6, 6,
-  7, 7, 7, 7,
-  8, 8, 8, 8,
-  9, 9, 9, 9,
-  10, 10, 10, 10,
-  'Jack', 'Jack', 'Jack', 'Jack',
-  'Queen', 'Queen', 'Queen', 'Queen',
-  'King', 'King', 'King', 'King',
-  'Ace', 'Ace', 'Ace', 'Ace'
-]
+STARTING_DECK = \
+[2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'] * 4
 BUST_LIMIT = 21
 STAY_THRESHOLD = BUST_LIMIT - 4
 MESSAGES = YAML.load_file("21_messages.yml")
